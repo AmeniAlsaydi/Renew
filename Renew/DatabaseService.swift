976143befe3c1d18deb0277ee5f973749ac504a9
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-05-11 17:10:45 -0400

    created main view story board added tab bar controller. created class to manage firebase database. created function to get categories.

---
+++ Renew/DatabaseService.swift
@@ -0,0 +1,36 @@
+//
+//  DatabaseService.swift
+//  Renew
+//
+//  Created by Amy Alsaydi on 5/11/20.
+//  Copyright © 2020 Amy Alsaydi. All rights reserved.
+//
+
+import Foundation
+import FirebaseFirestore
+import FirebaseAuth
+
+
+class DatabaseService {
+    public static let shared = DatabaseService()
+    private init() {}
+    
+    static let categoriesCollection = "materialCategories"
+    static let itemsCollection = "recyclableItems"
+    
+    private let db = Firestore.firestore()
+    
+    private func getCategories(completion: @escaping (Result<[Category], Error>) -> ()) {
+        
+        db.collection(DatabaseService.categoriesCollection).getDocuments { (snapshot, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else if let snapshot = snapshot {
+                let categories = snapshot.documents.map { Category($0.data())}
+                completion(.success(categories))
+            }
+        }
+    }
+    
+    
+}
