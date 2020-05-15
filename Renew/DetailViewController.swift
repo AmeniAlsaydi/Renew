6a5a75501192e4f0bbdf7bc81a358eb9a272f9b9
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-05-15 05:14:41 -0400

    completed the detail VC

--- Renew/DetailViewController.swift
+++ Renew/DetailViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import Kingfisher
 
 class DetailViewController: UIViewController {
     
     private var item: Item
+    
+    @IBOutlet weak var itemNameLabel: UILabel!
+    @IBOutlet weak var itemImage: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var prepLabel: UILabel!
     @IBOutlet weak var whyRecycleLabel: UILabel!
+    @IBOutlet weak var processLabel: UILabel!
     
     init?(coder: NSCoder, item: Item) {
         self.item = item
@@ -23,8 +29,6 @@ class DetailViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
@@ -33,7 +37,7 @@ class DetailViewController: UIViewController {
     
     private func getSteps() -> String {
         var steps = ""
-        
+        // Im thinking of having this part as a collection view they can swipe to see following steps instead of it just presented as a list.
         for i in 1...item.prepSteps.count {
             steps += "\(i). \(item.prepSteps[i - 1]) \n"
         }
@@ -54,8 +58,12 @@ class DetailViewController: UIViewController {
     
     private func updateUI() {
         
+        itemNameLabel.text = item.itemName
+        descriptionLabel.text = item.description
         prepLabel.text = getSteps()
         whyRecycleLabel.text = getReasons()
+        processLabel.text = item.recylcingProcess
+        itemImage.kf.setImage(with: URL(string: item.imageURL))
     }
     
 
@@ -64,4 +72,7 @@ class DetailViewController: UIViewController {
         sender.setImage(UIImage(systemName: "bookmark.fill"), for: .normal)
     }
     
+    @IBAction func dismissButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
 }
