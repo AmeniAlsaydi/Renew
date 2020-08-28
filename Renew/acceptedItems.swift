8e62278eaa72b90877c2dec427f9a2b2add5bc79
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-08-28 16:23:36 -0400

    added models for locations and accepted models

---
+++ Renew/acceptedItems.swift
@@ -0,0 +1,15 @@
+//
+//  acceptedItems.swift
+//  Renew
+//
+//  Created by Amy Alsaydi on 8/28/20.
+//  Copyright © 2020 Amy Alsaydi. All rights reserved.
+//
+
+import Foundation
+
+struct acceptedItems {
+    var itemName: String
+    var service: String
+    var notes: String?
+}
---
+++ Renew/recycleLocation.swift
@@ -0,0 +1,20 @@
+//
+//  recycleLocation.swift
+//  Renew
+//
+//  Created by Amy Alsaydi on 8/28/20.
+//  Copyright © 2020 Amy Alsaydi. All rights reserved.
+//
+
+import Foundation
+
+struct recycleLocation {
+    var name: String
+    var website: String?
+    var address: String?
+    var city: String?
+    var state: String?
+    var phoneNumber: String
+    var details: String
+    // there will also be a sub collection of accpetedItems 
+}
