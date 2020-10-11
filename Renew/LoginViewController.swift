6b7c0f6800cd3e162363b8cb03e7301a2e8f64fa
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-10-11 08:13:46 -0400

    added keyboard handling to log in VC

--- Renew/LoginViewController.swift
+++ Renew/LoginViewController.swift
@@ -16,6 +16,10 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var imageView: UIImageView!
     
+    @IBOutlet weak var imageViewWidthConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var aspectImage: NSLayoutConstraint!
+    
     lazy var textFields: [UITextField] = [emailTextField, passwordTextField]
     
     private lazy var tapGesture: UITapGestureRecognizer = {
@@ -28,9 +32,13 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         view.addGestureRecognizer(tapGesture)
         setUpUI()
-//        imageView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
-
+        registerForKeyboardNotifcations()
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        unregisterForKeyboardNotifcations()
+    } /// this can also be done in deinit
+
     
     /// logic to control the reappearance of the walkthrough screen -> should only display when user displays app for the very first time
     /// check user defaults "hasViewedWalkthrough" key to deternine if we should show again.
@@ -59,6 +67,39 @@ class LoginViewController: UIViewController {
     @objc private func didTap(_ gesture: UITapGestureRecognizer ) {
         emailTextField.resignFirstResponder()
         passwordTextField.resignFirstResponder()
+    }
+    
+    private func registerForKeyboardNotifcations() {
+        // singleton:
+        // add ourselevs as observer
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    private func unregisterForKeyboardNotifcations() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+    
+    @objc
+    private func keyboardWillShow(_ notification: NSNotification) {
+        imageViewWidthConstraint.constant = -100
+        
+        UIView.animate(withDuration: 5.0) { // FIX: no matter what duration i add it doesnt change how it happens? 
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    @objc
+    private func keyboardWillHide(_ notification: NSNotification) {
+        
+        imageViewWidthConstraint.constant = 0
+        
+        UIView.animate(withDuration: 5.0) {
+            self.view.layoutIfNeeded()
+        }
     }
     
     @IBAction func loginButtonPressed(_ sender: UIButton) {
