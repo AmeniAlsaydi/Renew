feafcdca3fef312bfe2f8a2fc4975fd55535f036
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-10-12 13:53:20 -0400

    updated the items VC from a table view to collection view

--- Renew/ItemsViewController.swift
+++ Renew/ItemsViewController.swift
@@ -12,8 +12,8 @@ class ItemsViewController: UIViewController {
     
     var category: Category?
     
-    @IBOutlet weak var tableView: UITableView!
-    
+    @IBOutlet weak var collectionView: UICollectionView!
+
     private var searchController: UISearchController!
     
     var items = [Item]()
@@ -25,7 +25,7 @@ class ItemsViewController: UIViewController {
     
     var filteredItems = [Item]() {
         didSet {
-            tableView.reloadData()
+            collectionView.reloadData()
         }
     }
     
@@ -39,10 +39,16 @@ class ItemsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureTableView()
         getItems()
         configureSearchController()
+        configureCollectionView()
         navigationItem.title = category?.materialType
+    }
+    
+    private func configureCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UINib(nibName: "SavedCell", bundle: nil), forCellWithReuseIdentifier: "savedCell")
     }
     
      private func configureSearchController() {
@@ -53,11 +59,6 @@ class ItemsViewController: UIViewController {
             searchController.searchBar.autocapitalizationType = .none
             searchController.obscuresBackgroundDuringPresentation = false
         }
-    
-    private func configureTableView() {
-        tableView.delegate = self
-        tableView.dataSource = self
-    }
     
     private func getItems() {
         DatabaseService.shared.getItems { [weak self] (results) in
@@ -71,40 +72,6 @@ class ItemsViewController: UIViewController {
         }
     }
 }
-
-extension ItemsViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return filteredItems.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
-        
-        let item = filteredItems[indexPath.row]
-        cell.textLabel?.text = item.itemName
-        
-        return cell
-    }
-}
-
-extension ItemsViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 50
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let item = items[indexPath.row]
-        
-        let storyboard = UIStoryboard(name: "MainView", bundle: nil)
-        let detailVC = storyboard.instantiateViewController(identifier: "DetailViewController") {
-            (coder) in
-            return DetailViewController(coder: coder, item: item)
-            
-        }
-//        present(detailVC, animated: true)
-        navigationController?.pushViewController(detailVC, animated: true)
-    }
-}
  
 extension ItemsViewController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
@@ -115,5 +82,52 @@ extension ItemsViewController: UISearchResultsUpdating {
         }
         searchText = text
         // upon assigning a new value to the searchText
+    }
+}
+
+
+extension ItemsViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let maxSize = UIScreen.main.bounds
+        
+        let height = maxSize.height * 0.11
+        let width = maxSize.width
+        
+        return CGSize(width: width, height: height)
+    }
+    
+    
+      func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+             return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+         }
+}
+
+extension ItemsViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return filteredItems.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "savedCell", for: indexPath) as? SavedCell else {
+            fatalError("could not dequeue cell to saved cell")
+        }
+        let item = filteredItems[indexPath.row]
+        cell.configureCell(item: item)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // TODO: display detail VC
+        let item = filteredItems[indexPath.row]
+        
+        let storyboard = UIStoryboard(name: "MainView", bundle: nil)
+        let detailVC = storyboard.instantiateViewController(identifier: "DetailViewController") {
+            (coder) in
+            return DetailViewController(coder: coder, item: item)
+            
+        }
+        navigationController?.pushViewController(detailVC, animated: true)
     }
 }
